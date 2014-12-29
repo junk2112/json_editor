724749c7b9f292d90dd359747ff93c7f64385fc3
junk2112 <zjunk58@gmail.com>
2014-12-30 00:03:35 +0300

    FIX original and result data showing, reading random files

--- bin/json_editor/editor/views.py
+++ bin/json_editor/editor/views.py
@@ -9,27 +9,47 @@ import json
 def home(request):
 	args = {}
 	args.update(csrf(request))
-
+	original_data = None
+	result_data = None
+	json_load_error = False
+	json_type = ''
+	json_content = ''
+	tags = ''
 	if request.method == 'POST':
-		json_data = request.FILES['uploaded_file'].read()
-		json_dict = json.loads(json_data)
-		args['json_type'] = json_dict['type']
-		args['json_content'] = json_dict['content']
-		tags = ''
-		for tag in json_dict['tags']:
-			tags = tags + tag + ' '
-		args['json_tags'] = tags
-		args['original_data'] = json_data
-		return render_to_response('editor.html', args)
+		splitted_name = str(request.FILES['uploaded_file']).split('.')
+		if splitted_name[-1].lower() != 'json':
+			json_load_error = True
+		else:
+			original_data = request.FILES['uploaded_file'].read()
+			json_dict = json.loads(original_data)
 
+			if 'type' in json_dict.keys():
+				json_type = json_dict['type']
+
+			if 'content' in json_dict.keys():
+				json_content = json_dict['content']
+
+			if 'tags' in json_dict.keys():
+				for tag in json_dict['tags']:
+					tags = tags + tag + ' '
+			args['json_tags'] = tags
+			args['json_type'] = json_type
+			args['json_content'] = json_content
+
+
+		
 	if request.method == 'GET':
 		form_type = request.GET.get('form_type', '')
 		form_tags = request.GET.get('form_tags', '')
 		form_content = request.GET.get('form_content', '')
 
-		tags = form_tags.split()
-		json_dict = {'type':form_type, 'tags':tags, 'content':form_content}
-		data = json.dumps(json_dict)
-		args['result_data'] = data
+		if form_type != '' and form_tags != '' and form_content != '':
+			tags = form_tags.split()
+			json_dict = {'type':form_type, 'tags':tags, 'content':form_content}
+			result_data = json.dumps(json_dict)
 
+	args['original_data'] = original_data
+	args['result_data'] = result_data
+	args['json_load_error'] = json_load_error
+	
 	return render_to_response('main.html', args)
