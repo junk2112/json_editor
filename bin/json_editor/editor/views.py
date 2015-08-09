ad92cd7a491d57e8b05581731a48195aea2f7237
junk2112 <zjunk58@gmail.com>
2015-08-09 16:00:01 +0300

    penalty editor

--- bin/json_editor/editor/views.py
+++ bin/json_editor/editor/views.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 from django.shortcuts import render_to_response, redirect
 from django.http import HttpResponse
 from django.core.context_processors import csrf
@@ -9,47 +11,71 @@ import json
 def home(request):
 	args = {}
 	args.update(csrf(request))
-	original_data = None
-	result_data = None
-	json_load_error = False
-	json_type = ''
-	json_content = ''
-	tags = ''
-	if request.method == 'POST':
-		splitted_name = str(request.FILES['uploaded_file']).split('.')
-		if splitted_name[-1].lower() != 'json':
-			json_load_error = True
-		else:
-			original_data = request.FILES['uploaded_file'].read()
-			json_dict = json.loads(original_data)
 
-			if 'type' in json_dict.keys():
-				json_type = json_dict['type']
+	# json_path = "/Users/andrew/git/json_editor/multipliers.json"
+	json_path = "/home/transfer/multipliers.json"
+	json_file = open(json_path, "r")
+	jdict = json.loads(json_file.read())
+	json_file.close()
 
-			if 'content' in json_dict.keys():
-				json_content = json_dict['content']
+	russian_names = {
+		"vechevaya": 	"Вещевая",
+		"dixi": 		"Дикси",
+		"nahabino": 	"Нахабино",
+		"taganka": 		"Таганка",
+		"himki": 		"Химки",
+		"trehgorka": 	"Трехгорка",
+		"ryazanka": 	"Рязанка",
+		"topolek": 		"Тополек",
+		"test": 		"Вещевая",
+		"lublino": 		"Люблино"
+	}
 
-			if 'tags' in json_dict.keys():
-				for tag in json_dict['tags']:
-					tags = tags + tag + ' '
-			args['json_tags'] = tags
-			args['json_type'] = json_type
-			args['json_content'] = json_content
-
-
+	points = []
+	for item in jdict:
+		points.append(
+			{
+				"penalty": jdict[item]["penalty"],
+				"penalty_input_name": "penalty_" + item,
+				"penalty_usd": jdict[item]["penalty_usd"],
+				"penalty_usd_input_name": "penalty_usd_" + item,
+				"max": jdict[item]["max"],
+				"max_input_name": "max_" + item,
+				"russian_name": russian_names[item],
+				"name":item
+			})
 		
-	if request.method == 'GET':
-		form_type = request.GET.get('form_type', '')
-		form_tags = request.GET.get('form_tags', '')
-		form_content = request.GET.get('form_content', '')
+	if request.method == 'POST':
+		post_data = {}
+		for p in points:
+			post_data[p["name"]] = {}
+			post_data[p["name"]]["penalty"] = 		float(request.POST.get(p["penalty_input_name"], 0))
+			post_data[p["name"]]["penalty_usd"] = 	float(request.POST.get(p["penalty_usd_input_name"], 0))
+			post_data[p["name"]]["max"] = 			float(request.POST.get(p["max_input_name"], 0))
 
-		if form_type != '' and form_tags != '' and form_content != '':
-			tags = form_tags.split()
-			json_dict = {'type':form_type, 'tags':tags, 'content':form_content}
-			result_data = json.dumps(json_dict)
+		for item in jdict:
+			jdict[item]["penalty"] = post_data[item]["penalty"]
+			jdict[item]["penalty_usd"] = post_data[item]["penalty_usd"]
+			jdict[item]["max"] = post_data[item]["max"]
+		
+		json_file = open(json_path, "w")
+		json_file.write(str(jdict).replace("u\'", "\'").replace("\'", "\""))
+		json_file.close()
 
-	args['original_data'] = original_data
-	args['result_data'] = result_data
-	args['json_load_error'] = json_load_error
+		points = []
+		for item in jdict:
+			points.append(
+				{
+					"penalty": jdict[item]["penalty"],
+					"penalty_input_name": "penalty_" + item,
+					"penalty_usd": jdict[item]["penalty_usd"],
+					"penalty_usd_input_name": "penalty_usd_" + item,
+					"max": jdict[item]["max"],
+					"max_input_name": "max_" + item,
+					"russian_name": russian_names[item],
+					"name":item
+				})
+
+	args["data"] = points
 	
 	return render_to_response('main.html', args)
